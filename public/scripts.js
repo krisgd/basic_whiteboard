7819d3b3bf5126e8658003f01afbc2b191599442
kris <krisdoromal@gmail.com>
2014-09-03 00:25:50 -0500

    first draft

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,103 @@
 /**
  * @author Kris Doromal
  */
+
+//SOCKET
+var socket;
+
+//EASELJS
+var stage;
+
+//DRAWING
+var drawingCanvas,
+	oldPt,
+	oldMidPt,
+	color = "#0000FF",
+	stroke = 4;
+
+//init
+$(function(){
+	//socketIO init
+	socket = io.connect("http://localhost:3000");
+	
+	//recieved from server
+	socket.on("drawDot", function(data){
+		drawDot(data);
+	});
+	
+	socket.on("drawLine",function(data){
+		drawLine(data.mpx, data.mpy, data.opx, data.opy, data.ompx, data.ompy);
+	});
+	
+	socket.on("clearDrawing", function(){
+		clearDrawing();
+	});
+		
+	
+	//easeljs init
+	stage = new createjs.Stage(document.getElementById("whiteboard"));
+	stage.enableMouseOver(10);
+	stage.mouseMoveOutside = false;
+	createjs.Touch.enable(stage);
+	stage.autoClear = false;
+	
+	//drawing init
+	drawingCanvas = new createjs.Shape();
+	stage.addChild(drawingCanvas);
+	stage.addEventListener("stagemousedown", mouseDownHandler);
+	stage.addEventListener("stagemouseup", mouseUpHandler);
+	
+	//clear button
+	$("#clear_button").click(function(){
+		//clear locally
+		clearDrawing();
+		//clear server broadcast to user
+		socket.emit("clearDrawing");
+	});
+});
+
+function mouseDownHandler(e){
+	oldPt = new createjs.Point(e.localX, e.localY);
+	oldMidPt = oldPt;
+	var midPt = new createjs.Point(oldPt.x + e.localX >> 1, oldPt.y + e.localY >> 1);
+	
+	//draw locally
+	drawDot(midPt);
+	//draw to server broadcast to other clients
+	socket.emit("drawDot", midPt);
+	
+	stage.addEventListener("stagemousemove", mouseMoveHandler);
+}
+
+function mouseMoveHandler(e){
+	var midPt = new createjs.Point(oldPt.x + e.localX >> 1, oldPt.y + e.localY >> 1);
+	
+	//draw locally
+	drawLine(midPt.x, midPt.y, oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
+	//draw to server broadcast to other clients
+	socket.emit("drawLine", {mpx:midPt.x, mpy:midPt.y, opx:oldPt.x, opy:oldPt.y, ompx:oldMidPt.x, ompy:oldMidPt.y});
+	
+	oldPt.x = e.localX;
+	oldPt.y = e.localY;
+	
+	oldMidPt.x = midPt.x;
+	oldMidPt.y = midPt.y;
+}
+
+function mouseUpHandler(){
+	stage.removeEventListener("stagemousemove", mouseMoveHandler);
+}
+
+function drawDot(mp){
+	drawingCanvas.graphics.clear().beginFill(color).drawCircle(mp.x, mp.y, stroke >> 1).endFill();
+	stage.update();
+}
+
+function drawLine(origX, origY, oldX, oldY, oldMidX, oldMidY){
+	drawingCanvas.graphics.clear().setStrokeStyle(stroke, "round", "round").beginStroke(color).moveTo(origX, origY).curveTo(oldX, oldY, oldMidX, oldMidY).endStroke();
+	stage.update();
+}
+
+function clearDrawing(){
+	stage.clear();
+}
--- server.js
+++ server.js
@@ -1,3 +1,32 @@
 /**
  * @author Kris Doromal
  */
+
+//required modules
+var io = require("socket.io"),
+	connect("connect");
+	
+//use connect to start a server   use function is used to connect to the public folder
+var app = connect().use(connect.static("public"))listen(3000, "0.0.0.0");
+
+//create websocket listen to the app made with connect
+var server = io.listen(app);
+
+//client connects
+server.sockets.on("connection", function(socket){
+	
+	//broadcast to users when dot is drawn
+	socket.on("drawDot", function(data){
+		socket.broadcast.emit("drawDot", data);
+	})
+	
+	//braodcast to users when line is drawn
+	socket.on("drawLine", function(data){
+		socket.broadcast.emit("drawLine", data);
+	})
+	
+	//broadcast to users when clear is called
+	socket.on("clearDrawing", function(){
+		socket.broadcast.emit("clearDrawing");
+	})
+});
